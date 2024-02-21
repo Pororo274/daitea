147f279be76b658be08d61bb64323fb7b52a7b36
Emil <emilzaripov48@gmail.com>
2024-02-22 00:41:49 +0300

    add create store product methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,17 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateProductRequest;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.index');
+        $query = Product::query();
+
+        if ($categoryId = $request->query('c')) {
+            $query->where('category_id', $categoryId);
+        }
+
+        $categories = Category::query()->get();
+        $products = $query->get();
+
+        return view('pages.index', [
+            'categories' => $categories,
+            'products' => $products,
+            'currentCategory' => +$categoryId
+        ]);
     }
 
     public function create()
     {
-        return view('');
+        return view('pages.products.create');
+    }
+
+    public function store(CreateProductRequest $request)
+    {
+        $imagePath = $request->file('image')->store('products');
+
+        Product::query()->create([
+            ...$request->only(['name', 'price', 'description']),
+            'image_path' => $imagePath
+        ]);
     }
 }
